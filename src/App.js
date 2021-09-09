99df103b834d773c3ab869395b2a3f99f4894b12
Viktorya <smirnovaviktorya1@gmail.com>
2021-09-09 13:42:24 +0300

    Add AuthorPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import PostsList from "./components/PostsList";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import PostPage from "./components/PostPage"
+import AuthorPage from "./components/AuthorPage";
+
 function App() {
   return (
     <div className="container">
-      <header className="text-center mt-5 mb-5">
-        <h1 className="text-uppercase pb-4">
-          <a href="/">Blog</a>
-        </h1>
-        <hr />
-      </header>
+      <Router>
+        <header className="text-center mt-5 mb-5">
+          <h1 className="text-uppercase pb-4">
+            <Link to="/">Blog</Link>
+          </h1>
+          <hr />
+        </header>
+
+        <Switch>
+          <Route exact path="/">
+            <PostsList />
+          </Route>
+          <Route path="/post/:id">
+            <PostPage />
+          </Route>
+          <Route path="/author/:id">
+            <AuthorPage />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
--- src/actions/getDataForAuthorPage.js
+++ src/actions/getDataForAuthorPage.js
@@ -4,8 +4,14 @@ async function getDataForAuthorPage(userId) {
   try {
     const data = await API.users.getById(userId);
     const posts = await API.posts.getByUserId(userId);
+    const photo = await API.albums.getPhotosById();
+    const { title, url } = photo[0];
     return {
       ...data,
+      photo: {
+        title,
+        url,
+      },
       posts,
     };
   } catch (error) {
